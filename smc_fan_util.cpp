ecde116c5014f4b77efe73a3d76020ae2f917d1c
Charlie Chiang <charlie_c_0129@outlook.com>
2020-07-22 20:48:53 +0800

    Edited headers.

--- smc_fan_util.cpp
+++ smc_fan_util.cpp
@@ -1,8 +1,8 @@
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
 #include <IOKit/IOKitLib.h>
 #include "smc.h"
 
